ea68152b19cdfcbe5ac26e93b6d297ea894ad480
dbvogel <dbvogelsang@gmail.com>
2023-06-19 11:30:25 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,145 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="calendar">
+    <div class="header"></div>
+    <div class="days">
+      <div v-for="(week, index) in calendarWeeks" :key="index" class="week">
+        <DayComponent
+          v-for="day in week"
+          :key="day.date"
+          :date="day.date"
+          :isSelected="day.isSelected"
+          :isStartDate="day.isStartDate"
+          :isEndDate="day.isEndDate"
+          @toggleSelection="toggleDaySelection(day)"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from 'vue'
+import DayComponent from './components/DayComponent.vue'
 
-export default {
-  name: 'App',
+export default defineComponent({
   components: {
-    HelloWorld
+    DayComponent
+  },
+  data() {
+    return {
+      calendarWeeks: []
+    }
+  },
+  created() {
+    this.generateCalendarWeeks()
+  },
+  methods: {
+    generateCalendarWeeks() {
+      const startDate = new Date(2023, 5, 1)
+      const endDate = new Date(2023, 5, 30)
+      const weeks = []
+      let currentDate = new Date(startDate)
+      currentDate.setDate(1)
+
+      while (currentDate <= endDate) {
+        const week = []
+        for (let i = 0; i < 7; i++) {
+          const date = new Date(currentDate)
+          week.push({
+            date: date.toISOString().slice(0, 10),
+            isSelected: false,
+            isStartDate: false,
+            isEndDate: false
+          })
+          currentDate.setDate(currentDate.getDate() + 1)
+        }
+        weeks.push(week)
+      }
+
+      this.calendarWeeks = weeks
+    },
+    toggleDaySelection(day) {
+      if (day.isSelected) {
+        this.clearSelection()
+      } else {
+        const selectedRange = this.calendarWeeks.flat().filter(d => d.isSelected || d.isStartDate || d.isEndDate)
+        const lastSelectedDay = selectedRange[selectedRange.length - 1]
+
+        if (lastSelectedDay && lastSelectedDay.isStartDate) {
+          const clickedDate = new Date(day.date)
+          const lastSelectedDate = new Date(lastSelectedDay.date)
+
+          if (clickedDate > lastSelectedDate) {
+            this.markRange(lastSelectedDate, clickedDate)
+          } else {
+            this.clearSelection()
+            day.isStartDate = true
+          }
+        } else {
+          day.isStartDate = true
+        }
+
+        day.isSelected = true
+      }
+
+      this.printSelectedRange()
+    },
+    markRange(startDate, endDate) {
+      this.clearSelection()
+
+      const start = new Date(startDate)
+      const end = new Date(endDate)
+
+      this.calendarWeeks.forEach(week => {
+        week.forEach(day => {
+          const currentDay = new Date(day.date)
+
+          if (currentDay >= start && currentDay <= end) {
+            day.isSelected = true
+
+            if (currentDay.getTime() === start.getTime()) {
+              day.isStartDate = true
+            }
+
+            if (currentDay.getTime() === end.getTime()) {
+              day.isEndDate = true
+            }
+          }
+        })
+      })
+    },
+    clearSelection() {
+      this.calendarWeeks.forEach(week => {
+        week.forEach(day => {
+          day.isSelected = false
+          day.isStartDate = false
+          day.isEndDate = false
+        })
+      })
+    },
+    printSelectedRange() {
+      const selectedRange = this.calendarWeeks
+        .flat()
+        .filter(day => day.isSelected)
+        .map(day => day.date)
+
+      if (selectedRange.length >= 2) {
+        const start = selectedRange[0]
+        const end = selectedRange[selectedRange.length - 1]
+        console.log('Ausgewählter Zeitraum:', start, 'bis', end)
+      }
+    }
   }
-}
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.calendar {
+  /* ... */
+}
+
+.week {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/DayComponent.vue
@@ -0,0 +1,76 @@
+<template>
+    <div
+      class="day"
+      :class="{ selected: isSelected, 'start-date': isStartDate, 'end-date': isEndDate }"
+      @click="handleClick"
+    >
+      <h2>{{ formattedDate }}</h2>
+      <ul>
+        <li v-for="event in events" :key="event.id">
+          <span>{{ event.title }}</span>
+          <span>{{ event.time }}</span>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue'
+  
+  export default defineComponent({
+    props: {
+      date: {
+        type: String,
+        required: true
+      },
+      isSelected: {
+        type: Boolean,
+        required: true
+      },
+      isStartDate: {
+        type: Boolean,
+        required: true
+      },
+      isEndDate: {
+        type: Boolean,
+        required: true
+      }
+    },
+    computed: {
+      formattedDate() {
+        return this.date
+      },
+      events() {
+        return [
+          { id: 1, title: 'Meeting', time: '09:00' },
+          { id: 2, title: 'Mittagessen', time: '12:30' },
+          { id: 3, title: 'Präsentation', time: '15:00' }
+        ]
+      }
+    },
+    methods: {
+      handleClick() {
+        this.$emit('toggleSelection')
+      }
+    }
+  })
+  </script>
+  
+  <style scoped>
+  .day {
+    /* ... */
+  }
+  
+  .day.selected {
+    background-color: red;
+  }
+  
+  .day.start-date {
+    /* ... */
+  }
+  
+  .day.end-date {
+    /* ... */
+  }
+  </style>
+  
